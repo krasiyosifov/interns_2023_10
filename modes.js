51e31c04315bc470e14e0ef623d840a9d07ad34d
Nikola Georgiev <nikolag2006@abv.bg>
2023-07-13 00:26:19 +0300

    mode fix

--- modes.js
+++ modes.js
@@ -1,12 +1,12 @@
 function normalMode() {
-    let modeButton = document.getElementById("modesButton").innerHTML = "Normal Mode";
+    let modeButton = document.getElementById("modesButton").innerHTML = "Normal";
     let prog = document.getElementById("progressClass");
 
     prog.classList.remove("d-none");
 }
 
 function extreamMode() {
-    let modeButton = document.getElementById("modesButton").innerHTML = "Extream Mode";
+    let modeButton = document.getElementById("modesButton").innerHTML = "Extream";
     let prog = document.getElementById("progressClass");
 
     prog.classList.add("d-none");
